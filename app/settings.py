3cc0b72fbca0ec63a783825a1deb656f93a019d9
ryzenboi98 <joaopro23@gmail.com>
2021-05-12 13:14:40 +0100

    Added static files configuration

--- app/settings.py
+++ app/settings.py
@@ -123,12 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_FILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
